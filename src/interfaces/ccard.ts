43e0f1c39a5851631f829714bbde2e2828e32292
fab89 <guratti.fabrizio@gmail.com>
2018-01-30 17:46:57 +0100

    add [deleteCCard] on [ProfilePage]

--- src/interfaces/ccard.ts
+++ src/interfaces/ccard.ts
@@ -1,14 +1,9 @@
 export class CCard {
+    id: number;
     intestatario: string;
     ncarta: number;
     dateCard: string;
     cardSecureCode: number;
     ccType: string;
-    constructor() {
-        this.intestatario = '';
-        this.ncarta = null;
-        this.dateCard = '';
-        this.cardSecureCode = null;
-        this.ccType = '';
-    }
+
 }
--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -11,14 +11,16 @@ import { CCard } from '../../interfaces/ccard';
 export class Profile {
 
   profileSegment = 'info';
-  cards = [{
-    intestatario: 'Fabrizio',
-    ncarta: 41111111111,
-    dateCard: '01/02/2020',
-    cardSecureCode: 123,
-    ccType: 'Visa'
+  cards: CCard[] = [{
+      id: 1,
+      intestatario: 'Fabrizio',
+      ncarta: 41111111111,
+      dateCard: '01/02/2020',
+      cardSecureCode: 123,
+      ccType: 'Visa'
     },
     {
+      id: 2,
       intestatario: 'Fabrizio2',
       ncarta: 3425555788,
       dateCard: '01/02/2022',
@@ -33,13 +35,20 @@ export class Profile {
 
   }
 
-  modalAddCreditCard(){
+  modalAddCreditCard(cards: CCard){
     let modal = this.modalCtrl.create(modalCCard);
     modal.onDidDismiss( data => {
+      data.id = this.cards.length +1;
       this.cards.push(data)
-      console.log(this.cards.length, 'data');
+      console.log(data.id, 'data');
     });
     modal.present();
+  }
+
+  deleteCCard(card){
+    let indexCCard = this.cards.indexOf(card);
+    this.cards.splice(indexCCard, 1)
+    console.log(indexCCard, 'carta eliminata')
   }
 
 }
